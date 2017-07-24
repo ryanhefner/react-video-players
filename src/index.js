0aaea7a978fdab72fc7c700775c53ef601c8ff04
Ryan Hefner <hi@ryanhefner.com>
2017-07-24 16:44:54 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * React Video Players
+ *
+ * @version 0.1.0
+ * @license MIT
+ * @author Ryan Hefner <hi@ryanhefner.com> (https://www.ryanhefner.com)
+ */
+
+import VideoPlayer from './VideoPlayer';
+import VimeoPlayer from './VimeoPlayer';
+import YouTubePlayer from './YouTubePlayer';
+import playerController from './playerController';
+
+export {
+  VideoPlayer,
+  VimeoPlayer,
+  YouTubePlayer,
+  playerController,
+};
