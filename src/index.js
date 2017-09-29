ae62ee2c387a40c16672f99a5b7f771ed1128991
Ryan Hefner <hi@ryanhefner.com>
2017-09-29 16:11:39 -0400

    Simplify index exports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-/**
- * React Video Players
- *
- * @version 0.1.0
- * @license MIT
- * @author Ryan Hefner <hi@ryanhefner.com> (https://www.ryanhefner.com)
- */
-
-import VideoPlayer from './VideoPlayer';
-import VimeoPlayer from './VimeoPlayer';
-import YouTubePlayer from './YouTubePlayer';
-import playerController from './playerController';
-
-export {
-  VideoPlayer,
-  VimeoPlayer,
-  YouTubePlayer,
-  playerController,
-};
+export { default as VideoPlayer } from './VideoPlayer';
+export { default as VimeoPlayer } from './VimeoPlayer';
+export { default as YouTubePlayer } from './YouTubePlayer';
+export { default as playerController } from './playerController';
